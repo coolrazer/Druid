237cf4ec9bdc10c125d3af69981c1843dc642089
kuying <ky@kuyingmatoMacBook-Pro.local>
2014-07-14 18:23:23 +0800

    init framework and bundle

---
+++ Druid.framework/Headers/DrArrayDataSource.h
@@ -0,0 +1,19 @@
+//
+//  DrArrayDataSource.h
+//  Druid
+//
+//  Created by kuying on 14-7-14.
+//  Copyright (c) 2014年 kuying. All rights reserved.
+//
+
+#import <UIKit/UITableView.h>
+
+typedef void (^TableViewCellConfigureBlock)(id cell, id item);
+
+@interface DrArrayDataSource : NSObject <UITableViewDataSource>
+
+- (id)initWithItems:(NSArray *)anItems cellIdentifier:(NSString *)aCellIdentifier configureCellBlock:(TableViewCellConfigureBlock)aConfigureCellBlock;
+
+- (id)itemAtIndexPath:(NSIndexPath *)indexPath;
+
+@end
---
+++ README.md
@@ -0,0 +1,11 @@
+Druid
+=======
+
+private iOS library, packing common codes
+
+cp.sh
+-----------------------
+```bash
+cp -r ${TARGET_DIR}/Druid.bundle ./
+cp -r ${TARGET_DIR}/Druid.framework ./
+```
